ca63f98e0384c0deb8f7c8332e133646cb5c9af0
Karen Cantrell <karen.izuka@gmail.com>
2026-04-26 19:58:01 -0700

    cleanup

--- weather-web-scraper/main.py
+++ weather-web-scraper/main.py
@@ -9,47 +9,66 @@ def main():
   try:
     #---set parameters---#
     final_df = pd.DataFrame()
-    start_date, end_date = '2020-01-01', '2020-12-31'
+    start_date, end_date = '2025-01-01', '2025-12-31'
 
     #https://www.ncdc.noaa.gov/cdo-web/token
     token = 'QOfbQjpzVcCmENdSHcKHvzCpxFoRgytS'
 
     #https://www.ncdc.noaa.gov/cdo-web/datatools/findstation
-    station_id = {'SF': 'GHCND:USW00023234',
-                  'Concord': 'GHCND:USW00023254',
-                  'LA': 'GHCND:USW00023174',
-                  'NYC': 'GHCND:USW00014732',
-                  'Seattle': 'GHCND:USW00024233',
-                  'Bend': 'GHCND:USS0021F21S'}
+    station_id = {
+                  'Lafayette': 'USR0000CBRI',
+                  'SF': 'USW00023234',
+                  'Oakland': 'USW00023230',
+                  'Livermore': 'USW00023285',
+                  'San Carlos': 'USC00047339',
+                  'San Jose': 'USW00023293',
+                }
 
     #loop through stations to generate weather df
     for i in station_id.keys():
       #instantiate empty lists
       dates = []
-      temps = []
+      max_temps = []
+      min_temps = []
 
       #get data from noaa and load into json file
-      r = requests.get(f'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=TAVG&limit=1000&stationid={station_id[i]}&startdate={start_date}&enddate={end_date}', headers={'token':token})
+      # r = requests.get(f'https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=PRCP&stations=US1CACC0045&startDate=2025-01-01&endDate=2025-01-31&includeAttributes=true&format=json', headers={'token':token})
+      r = requests.get(
+        'https://www.ncei.noaa.gov/access/services/data/v1',
+        params={
+            'dataset': 'daily-summaries',
+            'dataTypes': ['TMIN', 'TMAX'],
+            'stations': station_id[i],
+            'startDate': start_date,
+            'endDate': end_date,
+            'includeAttributes': 'true',
+            'format': 'json',
+            'units': 'metric'
+        },
+        headers={'token': token}
+      )
       d = json.loads(r.text)
 
       #extract dates and temperatures from json file
-      average_temps = [item for item in d['results'] if item['datatype']=='TAVG']
-      dates += [item['date'] for item in average_temps]
-      temps += [item['value'] for item in average_temps]
+      dates += [item['DATE'] for item in d if item.get('TMAX') is not None]
+      max_temps += [item['TMAX'] for item in d if item.get('TMAX') is not None]
+      min_temps += [item['TMIN'] for item in d if item.get('TMIN') is not None]
 
       #load dates and temperatures into df
       df = (pd.DataFrame()
-              .assign(date = [datetime.strptime(d, "%Y-%m-%dT%H:%M:%S") for d in dates])
-              .assign(temperature = [round(float(t)/10.0*1.8+32,2) for t in temps])
+              .assign(date = [datetime.strptime(d, "%Y-%m-%d") for d in dates])
+              .assign(max_temp = [round(float(t)*9/5+32,2) for t in max_temps])
+              .assign(min_temp = [round(float(t)*9/5+32,2) for t in min_temps])
+              .assign(temperature = lambda x: round((x['max_temp'] + x['min_temp'])/2, 2))
               .assign(index = lambda x: x.index)
               .assign(location = str(i))
               .reindex(columns=['index','location','date','temperature']))
-      
-      #append df to final_df
-      final_df = final_df.append(df, ignore_index=True)
+      print(df.head())
+      # append df to final_df
+      final_df = pd.concat([final_df, df], ignore_index=True)
 
     #output final df to csv
-    final_df.to_csv(f'/Users/karenizuka/Projects/python-scribbles/weather-web-scraper/data.csv', index=False)
+    final_df.to_csv(f'/Users/karencantrell/Documents/Projects/python-scribbles/weather-web-scraper/data.csv', index=False)
 
     #message box
     click.echo('Success! Weather extracted!')
